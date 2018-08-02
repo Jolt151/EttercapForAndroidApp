df3181b4da2cbfa3a4aee75f2a58882cb26c2391
michael <noemail>
2018-08-02 10:42:30 -0400

    clean up code more

---
+++ app/src/main/java/jolt151/ettercapforandroid/Util.java
@@ -0,0 +1,84 @@
+package jolt151.ettercapforandroid;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Created by michael on 8/2/2018.
+ */
+
+public class Util {
+    public static boolean unpackZip(String path, String zipname) {
+        InputStream is;
+        ZipInputStream zis;
+        try {
+            String filename;
+            is = new FileInputStream(path + zipname);
+            zis = new ZipInputStream(new BufferedInputStream(is));
+            ZipEntry ze;
+            byte[] buffer = new byte[1024];
+            int count;
+
+            while ((ze = zis.getNextEntry()) != null) {
+                // zapis do souboru
+                filename = ze.getName();
+
+                // Need to create directories if not exists, or
+                // it will generate an Exception...
+                if (ze.isDirectory()) {
+                    File fmd = new File(path + filename);
+                    fmd.mkdirs();
+                    continue;
+                }
+
+                FileOutputStream fout = new FileOutputStream(path + filename);
+
+                // cteni zipu a zapis
+                while ((count = zis.read(buffer)) != -1) {
+                    fout.write(buffer, 0, count);
+                }
+
+                fout.close();
+                zis.closeEntry();
+            }
+
+            zis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void copyFromAssetsToInternalStorage(String filename, Context context) {
+        AssetManager assetManager = context.getAssets();
+
+        try {
+            InputStream input = assetManager.open(filename);
+            OutputStream output = context.openFileOutput(filename, Context.MODE_PRIVATE);
+
+            copyFile(input, output);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+    }
+}
