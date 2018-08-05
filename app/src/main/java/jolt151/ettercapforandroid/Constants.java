c2f3abd4d380ffc94b107e64ea9fe3f7c2fc7757
michael <noemail>
2018-08-05 19:37:55 -0400

    Add a constant for output directory.

--- app/src/main/java/jolt151/ettercapforandroid/Constants.java
+++ app/src/main/java/jolt151/ettercapforandroid/Constants.java
@@ -1,5 +1,7 @@
 package jolt151.ettercapforandroid;
 
+import android.os.Environment;
+
 /**
  * Created by michael on 7/27/2018.
  */
@@ -7,4 +9,5 @@ package jolt151.ettercapforandroid;
 public class Constants {
     public static String FILES_DIR = "/data/data/jolt151.ettercapforandroid/files/";
     public static String CHMOD = "chmod 777 " + FILES_DIR + "EttercapForAndroid-master/bin/ettercap";
+    public static String OUTPUT_DIR = Environment.getExternalStorageDirectory() + "/EttercapForAndroid/";
 }
