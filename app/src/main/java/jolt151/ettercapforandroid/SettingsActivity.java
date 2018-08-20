a9939d8b76db494c267b0788f08f592d84e8703b
michael <noemail>
2018-08-19 21:17:10 -0400

    only allow changing settings if already purchased, if not prompt to upgrade

--- app/src/main/java/jolt151/ettercapforandroid/SettingsActivity.java
+++ app/src/main/java/jolt151/ettercapforandroid/SettingsActivity.java
@@ -1,5 +1,9 @@
 package jolt151.ettercapforandroid;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
@@ -7,43 +11,66 @@ import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceManager;
+import android.widget.Toast;
 
-public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+import com.anjlab.android.iab.v3.BillingProcessor;
+import com.anjlab.android.iab.v3.TransactionDetails;
+
+public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener,
+                                                                    BillingProcessor.IBillingHandler {
+
+    BillingProcessor billingProcessor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-/*        getSupportFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();*/
+        billingProcessor = new BillingProcessor(this, getString(R.string.license_key), this);
+
+
         addPreferencesFromResource(R.xml.preferences);
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         onSharedPreferenceChanged(sharedPreferences,getString(R.string.pref_default_args));
         onSharedPreferenceChanged(sharedPreferences,getString(R.string.pref_default_interface));
         onSharedPreferenceChanged(sharedPreferences,getString(R.string.pref_default_targets));
         sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+        if (!billingProcessor.isPurchased("fullversion")){
+            findPreference("default_args").setEnabled(false);
+            findPreference("default_interface").setEnabled(false);
+            findPreference("default_targets").setEnabled(false);
+
+            showDialog(0);
+        }
+
+
     }
-    
-/*    SharedPreferences.OnSharedPreferenceChangeListener listener =
-            new SharedPreferences.OnSharedPreferenceChangeListener() {
-                public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
-                    if (key.equals(prefs.getString("default_args",null))) {
-                        Preference default_args = findPreference(key);
-                        // Set summary to be the user-description for the selected value
-                        default_args.setSummary(prefs.getString(key, ""));
-                    }
-                    else if (key.equals(prefs.getString("default_interface",null))) {
-                        Preference default_interface = findPreference(key);
-                        default_interface.setSummary(prefs.getString(key,""));
-                    }
-                    else if (key.equals(prefs.getString("default_targets",null))) {
-                        Preference default_targets = findPreference(key);
-                        // Set summary to be the user-description for the selected value
-                        default_targets.setSummary(prefs.getString(key, ""));
-                    }
-                }
-            };*/
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == 0){
+            android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(SettingsActivity.this);
+            builder.setMessage("Upgrade to the full version to remove ads and gain the ability to output captured packets to a file and change default settings!")
+                    .setPositiveButton("Upgrade", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            if (!BillingProcessor.isIabServiceAvailable(getApplicationContext())){
+                                Toast.makeText(getApplicationContext(), "Purchasing has been disabled for your device", Toast.LENGTH_SHORT);
+                            } else{
+                                billingProcessor.purchase(SettingsActivity.this, "fullversion");
+                            }
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+            return builder.create();
+        }
+        return super.onCreateDialog(id);
+    }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
@@ -55,4 +82,43 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
         }
     }
 
+    //BillingHandler Implementation
+    @Override
+    public void onBillingInitialized() {
+        /*
+         * Called when BillingProcessor was initialized and it's ready to purchase
+         */
+    }
+
+    @Override
+    public void onProductPurchased(String productId, TransactionDetails details) {
+        /*
+         * Called when requested PRODUCT ID was successfully purchased
+         */
+    }
+
+    @Override
+    public void onBillingError(int errorCode, Throwable error) {
+        /*
+         * Called when some error occurred. See Constants class for more details
+         *
+         * Note - this includes handling the case where the user canceled the buy dialog:
+         * errorCode = Constants.BILLING_RESPONSE_RESULT_USER_CANCELED
+         */
+    }
+
+    @Override
+    public void onPurchaseHistoryRestored() {
+        /*
+         * Called when purchase history was restored and the list of all owned PRODUCT ID's
+         * was loaded from Google Play
+         */
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!billingProcessor.handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
